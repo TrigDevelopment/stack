2ac33f0dffa04411a5313e69eb7f77b0cbd1a283
TrigDeveloper1 <xx.artem-kokorin@mail.ru>
2016-09-11 15:48:07 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include "stack.h"
 stack<int> st;
+template <typename T>
+void outputStack(stack<T> _stack) {
+	while (_stack.count()) {
+		std::cout << _stack.pop() << " ";
+	}
+}
 void f1() {
 	int a = 8;
 	int b = 2;
-	st.push(b);
-	st.pop();
-	st.push(a);
+	st.push(1);
+	st.push(2);
+	stack<int> st2;
+	st2.push(8);
+	st2 = st;
+	outputStack(st2);
 }
-void outputStack() {
-	while (st.count()) {
-		std::cout << st.pop() << " ";
-	}
-}
+
 int main() {
 	f1();
-	outputStack();
+	outputStack(st);
 	system("pause");
 	return 0;
 }
